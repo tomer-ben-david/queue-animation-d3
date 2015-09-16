06058b4d66bd235ad8643811ca45c26ae9b4fd7e
radupcforever <radupcforever@gmail.com>
2015-09-16 18:16:33 +0300

    showing the empty slots

--- queue.js
+++ queue.js
@@ -1,13 +1,37 @@
 (function () {
     function updateQueueAnimation(animationQueue) {
-        var sel = d3.select("#queue_viewer")
-                .selectAll("div");
         
-        var d = sel.data(d3.range(1, animationQueue.occupancy + 1));
-        d.enter()
+		var filledSlots = animationQueue.occupancy;
+		var emptySlots = animationQueue.target_size - filledSlots;
+		
+		//console.log("filledSlots=" + filledSlots);
+		//console.log("emptySlots=" + emptySlots);
+		
+		// filled slots
+        var sel_filled = d3.select("#queue_viewer").selectAll("div.filled");
+        var d_filled = sel_filled.data(d3.range(1, filledSlots + 1));
+		d_filled
+			.attr("class", "filled")
+            .style("left", function(d) { return (d * (30 + 5)) + "px"; });
+        d_filled.enter()
             .append("div")
-            .style("left", function(d) { return (d * (20 + 5)) + "px"; });
-        d.exit()
+			.attr("class", "filled")
+            .style("left", function(d) { return (d * (30 + 5)) + "px"; });
+        d_filled.exit()
+            .remove();
+		
+		// empty slots
+		var sel_empty = d3.select("#queue_viewer").selectAll("div.empty");
+		var d_empty = sel_empty.data(d3.range(filledSlots + 1, animationQueue.target_size + 1));
+		d_empty
+			.attr("class", "empty")
+            .style("left", function(d) { return (d * (30 + 5)) + "px"; });
+
+        d_empty.enter()
+            .append("div")
+			.attr("class", "empty")
+            .style("left", function(d) { return (d * (30 + 5)) + "px"; });
+        d_empty.exit()
             .remove();
     }
     
@@ -21,7 +45,7 @@
           [{
             "name": "Queue Name",
             "occupancy": Math.floor(Math.random() * 10), // max 10 elements in queue
-            "target-size": 10
+            "target_size": 10
           }]
         };
 		
@@ -47,7 +71,7 @@
 		updateQueueAnimation(aq);
 	}
 	
-    setInterval(
+    var interval = setInterval(
         function () {
             run();
         },
