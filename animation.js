8080d890248912face0e2686e2f477df2e219d87
radupcforever <radupcforever@gmail.com>
2015-10-02 01:35:56 +0300

    - queue name extracted from response - show errors, show empty queue message

--- animation.js
+++ animation.js
@@ -1,6 +1,11 @@
 var animation = animation || (function () {
     return {
         updateQueueAnimation: function(animationQueue) {
+			if(animationQueue.error) {
+				d3.select("#queue_error").text("Error: " + animationQueue.error);
+				return;
+			}
+			
             var occupancy = animationQueue["occupancy"];
 			var viewer_width = 1 * (d3.select("#queue_viewer").style("width").replace("px", ""));
 			var slot_width = 30;	// the width of a square representing a slot
@@ -8,21 +13,26 @@ var animation = animation || (function () {
 			
             //console.log("filledSlots=" + filledSlots);
             //console.log("emptySlots=" + emptySlots);
-			
+			d3.select("#queue_name").text(animationQueue.name);
+
             // filled slots
             var sel_filled = d3.select("#queue_viewer").selectAll("div.filled");
 			var current_occupancy = 0;
 			
+			// a d3 selection is an array. The first element of the array contains the selected elements
 			if(sel_filled[0][0]) {
 				if(sel_filled[0][0].constructor === Array) {
+					// if the first element of the d3 selecton is an array, it means there are more than 1 elements selected
 					current_occupancy = sel_filled[0][0].length;
 				}
 				else {
+					// if the first element of the d3 selection is _not_ an array, it means there is only 1 element selected
 					current_occupancy = 1;
 				}
 			}
 			
 			if(current_occupancy > occupancy) {
+				// set some values that will force the surplus divs to exit
 				sel_filled.datum(function(d, i) {
 					return d - (occupancy - current_occupancy);
 				});
@@ -45,6 +55,13 @@ var animation = animation || (function () {
 				.transition()
 				.style("left", function(d) { return viewer_width + "px"; })
 				.remove();
+			
+			if(occupancy == 0) {
+				d3.select("#queue_info").text("Empty queue");
+			}
+			else {
+				d3.select("#queue_info").text("");
+			}
         },
 
         /*
