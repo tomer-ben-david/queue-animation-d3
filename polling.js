26d2fe452e7cc400ba1878595e0093a8b7212c23
radupcforever <radupcforever@gmail.com>
2015-10-12 23:02:38 +0300

    using jsonp

--- polling.js
+++ polling.js
@@ -1,42 +1,48 @@
 var polling = polling || (function () {
     var _url;
     var _pollingInterval;
-    var _callback;
     
 	var count = 0;
 	var intervaId;
 	
-    function internalCallback(error, response) {
-        if(!error || typeof(error) == "undefined") {
-            _callback(response);
-        }
-        else {
-			if(error.status = 404) {
-				_callback({
-					"error": _url + " not found"
-				});
-			}
-			else {
-				_callback({
-					"error": error.response
-				});
-			}
-            console.log(error);
-        }
-    }
-    
+	function pollError(err) {
+		console.error(err);
+		
+		d3.select(this).remove();
+	}
+	
+	function pollLoaded() {
+		d3.select(this).remove();
+	}
+	
+	// function called at each tick for requesting new queue info
+	// this function uses jsonp approach in order to get data from other domains
+	// it creates a script node, attaches the error and onload events, attaches the script to the head node
+	// sets the url
+	// on both events the script is removed from DOM
+	function poll() {
+		var script = document.createElement("script");
+		script.type = "text/javascript";
+		script.onerror = pollError;
+		script.onload = pollLoaded;
+		
+		document.getElementsByTagName("head")[0].appendChild(script);
+		
+		script.src = _url;
+	}
+
     return {
-        init: function(url, pollingInterval, callback) {
+        init: function(url, pollingInterval) {
             _url = url;
             _pollingInterval = pollingInterval;
-            _callback = callback;
         },
 
+		
         start: function() {
-            d3.json(_url, internalCallback);
+            poll();
 
             intervalId = setInterval(function() {
-                            d3.json(_url, internalCallback);
+                            poll();
                        },
                        _pollingInterval);
         }
---
+++ test/response.js
@@ -0,0 +1,23 @@
+// http://stackoverflow.com/questions/403967/how-may-i-reference-the-script-tag-that-loaded-the-currently-executing-script
+
+
+
+queueAnimationJsonpCallback(
+	{
+	  "keynamevalue": {
+		"subkeyvalue-a": "queue-name",
+		"subkeyvalue-b": "queue-sub-category",
+		"subkeyvalue-c": 3
+	  },
+	  "queuedatavalue": {
+		"some other element": 454545,
+		"occupancydatavalue": {
+		  "some other element": "fdkgfkjfkj",
+		  "occupancyvalueval": 0
+		}
+	  },
+	  "targetqueuesizedatavalue": {
+		"some other element": 454545,
+		"targetqueuesizevalue": 5.0
+	  }
+	});
